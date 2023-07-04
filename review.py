af1be45ab40a3ae2fd660b3d47f1e080771488b3
Sheen <9674168+sheenhx@users.noreply.github.com>
2023-07-04 21:21:27 +0200

    all: Update review.py

--- review.py
+++ review.py
@@ -7,7 +7,7 @@ def translate(text, target_language):
     openai.api_key = os.getenv("OPENAI_API_KEY")
     openai.organization = os.getenv("OPENAI_ORG_KEY")
     complete_prompt = '''    
-    Act as professional translator in crypto, use json in response to translate the crypto app json from english to target language (ISO 3166 country codes).remove all unnecessary comment just return back json.Plesae make sure you return the all JSON key/value, don't miss any key/value.
+    Act as professional translator in crypto, use json in response to translate the crypto app json from english to target language (ISO 3166 country codes).remove all unnecessary comment in response just return back json.Plesae make sure you return the all JSON key/value, key formats should be exactly the same.
     The targeted language:    
     '''
     prompt = complete_prompt + target_language + text
