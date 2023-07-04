1b09706717b7e3802f1eb3b3eeb07058971619ce
Sheen <9674168+sheenhx@users.noreply.github.com>
2023-07-04 21:01:39 +0200

    all: Update review.py

--- review.py
+++ review.py
@@ -7,10 +7,10 @@ def translate(text, target_language):
     openai.api_key = os.getenv("OPENAI_API_KEY")
     openai.organization = os.getenv("OPENAI_ORG_KEY")
     complete_prompt = '''    
-    Act as professional translator in crypto, use json in response to translate the crypto app json from english to target language (ISO 3166 country codes). Try to find as many problems as possible.
-    The english Json for translate are below:    
+    Act as professional translator in crypto, use json in response to translate the crypto app json from english to target language (ISO 3166 country codes).remove all unnecessary comment just return back json.
+    The targeted language:    
     '''
-    prompt = complete_prompt + text
+    prompt = complete_prompt + target_language + text
 
     print(f"\nPrompt in full sent to GPT-4: {prompt}\n")
 
@@ -47,32 +47,26 @@ def translate(text, target_language):
     return review
 
 def main():
-    # Directory where the JSON files are stored
-    directory = "assets/flutter_i18n"
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    openai.organization = os.getenv("OPENAI_ORG_KEY")
+    directory = 'assets/flutter_i18n'
+    languages = ['de', 'zh_CN']  # Add the languages you want to translate to here
 
-    # Load English JSON
+    # Load the English JSON
     with open(f'{directory}/en.json', 'r') as f:
         data_en = json.load(f)
 
-    # Languages to translate to
-    languages = ['cn', 'de']
-
+    # Translate the entire English JSON to each target language
     for lang in languages:
-        # Load the target language file if it exists
-        try:
-            with open(f'{directory}/{lang}.json', 'r') as f:
-                data_target = json.load(f)
-        except FileNotFoundError:
-            data_target = {}
+        # Convert JSON to string and translate it
+        json_str = json.dumps(data_en, indent=4, ensure_ascii=False)
+        translated_json_str = translate(json_str, lang)
 
-        # Translate each entry in the English JSON that's not present in the target language file
-        for k, v in data_en.items():
-            if k not in data_target or not data_target[k]:
-                data_target[k] = translate(v, lang)
+        # Convert translated string back to JSON and save it
+        data_translated = json.loads(translated_json_str)
 
-        # Save the translated JSON
-        with open(f'{directory}/{lang}.json', 'w') as f:
-            json.dump(data_target, f, ensure_ascii=False, indent=4)
+        with open(f'{directory}/{lang}.json', 'w', encoding='utf-8') as f:
+            json.dump(data_translated, f, ensure_ascii=False, indent=4)
 
 if __name__ == "__main__":
     main()
